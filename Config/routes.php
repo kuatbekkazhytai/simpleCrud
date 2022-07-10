14c6dedc08ce841e3dbd3b74b3b2c766fc047dfa
kuatbek.kazhitai <kuatbek.kazhitai@tulip.com>
2022-07-11 00:35:46 +0600

    Composer and autoloader introduced to project

--- Config/routes.php
+++ Config/routes.php
@@ -1,12 +1,10 @@
 <?php
 
-include_once dirname(__FILE__) . '/RouteParser.php';
-
-use App\config\RouteParser;
+use App\Config\RouteParser;
 use App\Controllers\EmployeeController;
 
-RouteParser::add('GET', '/employees',[EmployeeController::class, 'index']);
-RouteParser::add('GET', '/employees/{id}',[EmployeeController::class, 'show']);
-RouteParser::add('POST', '/employees/create',[EmployeeController::class, 'store']);
-RouteParser::add('PUT', '/employees/update',[EmployeeController::class, 'update']);
-RouteParser::add('DELETE', '/employees/delete',[EmployeeController::class, 'delete']);
+RouteParser::add('GET', '/employees', [EmployeeController::class, 'index']);
+RouteParser::add('GET', '/employees/{id}', [EmployeeController::class, 'show']);
+RouteParser::add('POST', '/employees/create', [EmployeeController::class, 'store']);
+RouteParser::add('PUT', '/employees/update', [EmployeeController::class, 'update']);
+RouteParser::add('DELETE', '/employees/delete', [EmployeeController::class, 'delete']);
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
 include_once dirname(__FILE__) . '/Config/routes.php';
